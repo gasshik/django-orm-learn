fae7d01d5ff16095e2d4e34b59bc5bfff92a73ef
gasshik <gashik7656@gmail.com>
2025-10-19 13:53:49 +0300

    Create Task model using Django ORM

--- lesson/models.py
+++ lesson/models.py
@@ -8,3 +8,19 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description =  models.TextField()
+    status = models.CharField(max_length=20, choices=[
+        ("in progress", "In progress"),
+        ("completed", "Completed"),
+        ("postponed", "Postponed"), 
+    
+    ], default="in progress"
+    )
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null = True, blank=True)
+
+def __str__(self):
+    return f"{self.title} - {self.status}"
